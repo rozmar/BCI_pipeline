aa338feb6ca45a9c288ba44eef78a5ecedacf205
rozmar <qtyush@gmail.com>
2021-03-25 23:13:07 -0400

    loading wavesurfer files

---
+++ utils/utils_ephys.py
@@ -0,0 +1,74 @@
+from pywavesurfer import ws
+import numpy as np
+import time as timer
+import datetime
+
+def recover_digital_channels(array_raw,channelnum):
+    digitalarrays = list()
+    for i in np.arange(channelnum)[::-1]:
+        array_now = np.zeros_like(array_raw)
+        array_now[array_raw>=2**i] = 1
+        array_raw[array_raw>=2**i] -=2**i
+        digitalarrays.append(np.asarray(array_now,int))
+    digitalarrays = np.concatenate(digitalarrays[::-1])
+    return digitalarrays
+
+def load_wavesurfer_file(WS_path): # works only for single sweep files
+    #%
+    ws_data = ws.loadDataFile(filename=WS_path, format_string='double' )
+    #units = np.array(ws_data['header']['AIChannelUnits']).astype(str)
+    channelnames_analog = np.array(ws_data['header']['AIChannelNames']).astype(str)
+    activechannels_analog = np.concatenate(ws_data['header']['IsAIChannelActive'])==1
+    channelnames_analog = channelnames_analog[activechannels_analog]
+    channelnames_digital = np.array(ws_data['header']['DIChannelNames']).astype(str)
+    activechannels_digital = np.concatenate(ws_data['header']['IsDIChannelActive'])==1
+    channelnames_digital = channelnames_digital[activechannels_digital]
+    keys = list(ws_data.keys())
+    del keys[keys=='header']
+    if len(keys)>1:
+        print('MULTIPLE SWEEPS! HANDLE ME!! : {}'.format(WS_path))
+        timer.sleep(10000)
+    sweep = ws_data[keys[0]]
+    sRate = ws_data['header']['AcquisitionSampleRate'][0][0]
+    timestamp = ws_data['header']['ClockAtRunStart']
+    outdict = {'sampling_rate':sRate,
+               'timestamp':timestamp,
+               'sweep_timestamp':sweep['timestamp'][0][0]}
+    digitalarrays = recover_digital_channels(sweep['digitalScans'],len(channelnames_digital))
+    for idx,name in enumerate(channelnames_analog):
+        outdict['AI-{}'.format(name)] = sweep['analogScans'][idx,:]
+    for idx,name in enumerate(channelnames_digital):
+        outdict['DI-{}'.format(name)] = digitalarrays[idx,:]
+    seconds = np.floor(outdict['timestamp'][5]+outdict['sweep_timestamp'])
+    microseconds = (outdict['timestamp'][5]+outdict['sweep_timestamp']-seconds)*1000000
+    outdict['sweep_start_timestamp'] = datetime.datetime(outdict['timestamp'][0],outdict['timestamp'][1],outdict['timestamp'][2],outdict['timestamp'][3],outdict['timestamp'][4],seconds,microseconds)
+
+    return outdict
+
+def decode_bitcode(ephysdata):
+    sampling_rate = ephysdata['sampling_rate']
+    trial_start_idxs = np.where(np.diff(ephysdata['DI-TrialStart'])==1)[0]
+    trial_end_idxs = np.where(np.diff(ephysdata['DI-TrialStart'])==-1)[0]
+    bitcode_trial_nums = list()
+    for trial_start_idx,trial_end_idx in zip(trial_start_idxs,trial_end_idxs):
+        bitcode_channel_now = (ephysdata['AI-Bitcode'][trial_start_idx:trial_end_idx]>3)*1
+        pulse_start_idx = np.where(np.diff(bitcode_channel_now)==1)[0]
+        pulse_end_idx = np.where(np.diff(bitcode_channel_now)==-1)[0]
+        pulse_lengths = list()
+        for pulse_start,pulse_end in zip(pulse_start_idx,pulse_end_idx):
+            pulse_lengths.append((pulse_end-pulse_start)/sampling_rate)
+        pulse_lengths = np.asarray(pulse_lengths)
+        digits = ''
+        while len(pulse_lengths)>0:
+            if len(pulse_lengths[:np.argmax(pulse_lengths[1:]<.01)+2]) == 3:
+                digits += '1'
+            else:
+                digits += '0'
+            pulse_lengths = pulse_lengths[np.argmax(pulse_lengths[1:]<.01)+2:]
+        if len(digits)<11:
+            bitcode_trial_nums.append(np.nan)
+        else:
+            bitcode_trial_nums.append(int(digits,2))
+    
+    ephysdata['bitcode_trial_nums'] = bitcode_trial_nums
+    return ephysdata
