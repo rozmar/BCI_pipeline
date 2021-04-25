bb670dd262066d5d14b3253b1b26ae687e5cbd5f
rozmar <qtyush@gmail.com>
2021-04-25 10:34:36 -0400

    bad function was there

--- export_populate_main.py
+++ export_populate_main.py
@@ -1,6 +1,7 @@
 from pipeline.ingest import datapipeline_metadata, datapipeline_experiment
 from utils import utils_pipeline
 
-utils_pipeline.extract_files_from_dir() # exports bpod sessions
+utils_pipeline.export_pybpod_files() # exports bpod sessions
+#%%
 datapipeline_metadata.populatemetadata(updatemetadata = True ) # update google metadata and upload lab schema
 datapipeline_experiment.populate_behavior() # populate experiment schema
